531482115fbaea57d282e0b758c3901d45ffbd80
Tim Webster <timothy.webster@gmail.com>
2011-05-10 23:06:20 -0400

    now supports boric acid

--- chemistry/chemistry.clj
+++ chemistry/chemistry.clj
@@ -1,7 +1,7 @@
 (require 'clojure.contrib.string)
 
 ; split a single compound into a list of elements and subscripts
-(defn chem-split [s] (re-seq #"[A-Z][a-z]*|\d+" s))
+(defn chem-split [s] (re-seq #"[A-Z][a-z]*|\d+|\(.+\)\d+" s))
 
 ; parse a string into an int if it looks like an int, otherwise leave it be
 (defn soft-parse [s] (cond (re-matches #"\d+" s) (Integer/parseInt s) :else s))
@@ -11,6 +11,23 @@
 
 ; sugar for merge-with
 (defn  addKey [x y h] (merge-with + (hash-map x y) h))
+
+; expand a functional group (i.e., (OH)3) into simple representation
+(defn expandFG [s]
+(let
+[parts  (clojure.contrib.string/split #"\(|\)" s)
+ group (nth parts 1) 
+ subscript (Integer/parseInt (nth parts 2))]
+(reduce str ( replicate subscript group))))
+
+; only expand a string if it appears to be a functional group
+(defn soft-expand [s] (cond (. s startsWith "(") (expandFG s) :else s))
+
+; expand functional groups in a compound string 
+(defn expand-all [s]
+  (cond (not (re-matches #".*\(.+\)\d+.*" s)) s
+   :else 
+(recur (reduce str (map soft-expand (re-seq #"\(.+?\)\d+|.+?" s))))))
 
 ; convert a compound (in list format) into a hashtable with element symbols as keys
 (defn listToHash [l] 
@@ -29,7 +46,9 @@
 ; take a compound string, split it into a list, convert int strings to ints, and convert the list to a hashtable
 (defn chem2hash [s]
 	(->>
-	  (chem-split s)
+   (expand-all s)
+   (soft-expand)
+	  (chem-split)
 	  (map soft-parse)
 	  (listToHash)))
 
@@ -53,4 +72,4 @@
       (println (str s " " (balanced? s))))
 
 ;run an example
-(my-test "H2O=H2O")
+(my-test "10Unn+10Unn=20Unn")
